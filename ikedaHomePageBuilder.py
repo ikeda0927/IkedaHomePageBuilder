3f473cb467c76d69c3315bea34defca71c68cf14
ikeda0927 <ikeda.kohei4477@gmail.com>
2019-09-14 14:34:53 +0900

    incomplete ver 0.3

--- ikedaHomePageBuilder.py
+++ ikedaHomePageBuilder.py
@@ -3,9 +3,10 @@ import tkinter as tk
 base=tk.Tk()
 frame=tk.Frame(base)
 entryList = list()
+stringList = list()
 
 class Entry:
-    type=""
+    type=None
     entry=None
     sv=tk.StringVar()
     def __init__(self,base, type):
@@ -23,9 +24,9 @@ class Entry:
             self.entry=tk.Entry(base,textvariable=sv)
 
     def writeToFile(self):
-        if type=="p":
+        if self.type=='p':
             string=self.entry.get('1.0', 'end -1c')
-        elif type=="code":
+        elif self.type=='code':
             string=self.entry.get('1.0', 'end -1c')
         else:
             string=self.sv.get()
@@ -36,14 +37,24 @@ class Entry:
 
     def hide(self):
         print("hide is called")
-        self.entry.lower()
+        stringList.append(self.writeToFile())
+        for s in stringList:
+            print(s)
+            print('\n')
+        self.entry.destroy()
 
 def makeFile():
+    if len(entryList) !=0:
+        last=entryList[-1]
+        last.hide()
     with open("test.txt",'a')as f:
-        for e in entryList:
-            f.write(e.writeToFile())
+        for s in stringList:
+            f.write(s)
             f.flush()
     base.destroy()
+
+def buttonAction(entry):
+    entry.hide()
 
 def makeEntry(string):
     # print("hello"+str)
@@ -59,8 +70,8 @@ def makeEntry(string):
 
 if __name__ == '__main__':
     frame.pack()
-    textBox= tk.Text(frame)
-    textBox.pack()
+    # textBox= tk.Text(frame)
+    # textBox.pack()
     sv1 = tk.StringVar()
     sv1.set('')
     option= tk.OptionMenu(base,sv1, 'h1', 'h2','h3','h4','h5','p','code',command=makeEntry).pack()
